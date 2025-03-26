233c914c70290326300f7d61fe9eeba8c4decee6
thelightway <thelightway@daum.net>
2025-03-26 17:10:18 +0900

    Refactor: messageTempleate 사용 방법 수정

--- src/main/java/com/inspire12/likelionwebsocket/controller/ChatAdminController.java
+++ src/main/java/com/inspire12/likelionwebsocket/controller/ChatAdminController.java
@@ -2,7 +2,6 @@ package com.inspire12.likelionwebsocket.controller;
 
 import com.inspire12.likelionwebsocket.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +14,7 @@ public class ChatAdminController {
 
     @PostMapping("/call")
     public ChatMessage sendMessage(@RequestBody ChatMessage chatMessage){
-        messagingTemplate.setDefaultDestination("/topic/public");
-        messagingTemplate.convertAndSend(chatMessage);
+        messagingTemplate.convertAndSend("/topic/public", chatMessage);
         return chatMessage;
     }
 }
